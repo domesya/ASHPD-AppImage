e92e1b84eb18e23aa34c28f95ed44e8a233bf257
domesya <163451587+domesya@users.noreply.github.com>
2026-05-22 17:53:31 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ashpd-demo | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/bilelmoussaoui/ashpd/refs/heads/main/demo/client/data/icons/com.belmoussaoui.ashpd.demo.svg
+export DESKTOP=/usr/share/applications/com.belmoussaoui.ashpd.demo.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/ashpd-demo
 
 # Additional changes can be done in between here
 
