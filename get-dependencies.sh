3e724ed92bce298d2b3e90f51ae489fa1a7e8a06
domesya <163451587+domesya@users.noreply.github.com>
2026-05-22 18:17:29 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,6 +14,8 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+# wth
+export BINDGEN_EXTRA_CLANG_ARGS="-F/usr/include"
 make-aur-package ashpd-demo
 
 # If the application needs to be manually built that has to be done down here
