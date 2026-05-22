5abcef2c60b9fa9576ebcd2464913c17fd7f4fc4
domesya <163451587+domesya@users.noreply.github.com>
2026-05-22 18:26:33 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,8 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-  clang
+  clang \
+  llvm20
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
